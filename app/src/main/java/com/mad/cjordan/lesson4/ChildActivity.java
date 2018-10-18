e53e302eee783f5666388147fb15d2281e5f3c68
Koravio <cjordan7857@lions.eastms.edu>
2018-10-18 10:31:58 -0500

    Initial commit

---
+++ app/src/main/java/com/mad/cjordan/lesson4/ChildActivity.java
@@ -0,0 +1,16 @@
+package com.mad.cjordan.lesson4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+    private TextView mDisplayText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+    }
+}
